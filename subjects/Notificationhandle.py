ca25f8338a4c9a8a268f39d117ea9c694eeca35b
MercyAkpan <akpanmeme@gmail.com>
2026-08-21 19:54:00 +0100

    Updated the file structure for these files

--- subjects/Notificationhandle.py
+++
@@ -1,8 +0,0 @@
-class NotificationHandle:
-    def __init__(self, observer):
-        self.observer = observer
-        self.observer_name = observer.__class__.__name__
-
-    def update_observer(self, observer):
-        observer.update()
-
